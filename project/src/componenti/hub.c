d8a07a477c9fcf8d43bcd332f1215406a4ef23d5
Claudio Facchinetti <devack@hotmail.it>
2019-05-09 11:54:27 +0200

    Aggiunti alcuni import e aggiunti i commenti. Divisa la gestione in funzioni.

--- project/src/componenti/hub.c
+++ project/src/componenti/hub.c
@@ -4,32 +4,109 @@
 #include <time.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "strutture_dati/tipi_componente.h"
 #include "strutture_dati/coda_stringhe.h"
 #include "comunicazione/comunicazione.h"
 
+/*
+* L'id univoco del componente.
+*/
 int id;
+
+/*
+* Lista che contiene le pipes dei figli.
+*/
 lista_stringhe* lista_pipes;
+
+/*
+* pipe_interna: FIFO per le comunicazioni interne al processo.
+* pipe_esterna: FIFO per le comunicazioni con l'umano.
+*/
 char pipe_interna[50];
 char pipe_esterna[50];
+
+/*
+* I process ID dei processi di supporto figli.
+*/
 pid_t figli[2];
 
+/*
+* Funzione che calcola il valore di un registro intero.
+* r: registro da calcolare
+* res: variabile in cui verrà salvato il valore.
+* Return: FALSE in caso di errori, TRUE altrimenti.
+*/
 boolean calcola_registro_intero( const registro* r, int* res );
+
+/*
+* Funzione che calcola il valore di un registro di tipo stringa.
+* r: registro da calcolare
+* output: variabile in cui verrà salvato il valore.
+* Return: FALSE in caso di errori, TRUE altrimenti.
+*/
 boolean calcola_registro_stringa( const registro* r, string output);
+
+/*
+* Funzione che sta sempre in ascolto sulla FIFO interna ed interpreta i comandi.
+*/
 void ascolta_e_interpreta();
+
+/*
+* Fuzione che gestisce il messaggio per avere li stato.
+*/
+void gestisci_STATUSGET(coda_stringhe* separata);
+
+/*
+* Funzione che gestisce il comando per la verifica dell'ID.
+*/
+void gestisci_ID(coda_stringhe* separata);
+
+/*
+* Funzione che gestisce il comando per generare nuovi figli.
+*/
+void gestisci_SPAWN(coda_stringhe* separata);
+
+/*
+* Funzione che gestisce il comando per rimuovere un componente
+*/
+void gestisci_REMOVE(coda_stringhe* separata);
+
+/*
+* Funzione che gestisce il comando per l'aggiornamento di un interruttore.
+*/
+void gestisci_LABELUP(coda_stringhe* separata);
+
+/*
+* Funzione per terminare il processo quando arriva un SIGINT.
+*/
 void termina(int x);
+
+/*
+* Funzione che crea i processi per gestire le più pipe.
+*/
 void crea_processi_supporto();
 
 int main( int argn, char** argv ){
 
+  /*
+  * Leggo l'id dalla riga fi comando.
+  */
   if( argn < 2 )
-    exit(136);
+    exit(130);
   id = atoi(argv[1]);
 
+  /*
+  * Inizializzo la lista delle PIPE dei miei figli.
+  */
   lista_pipes = crea_lista();
   append(lista_pipes, "/tmp/10");
 
+  /*
+  * Creo le variabili che contengono i percorsi delle PIPE.
+  */
   sprintf(pipe_interna, "%s/%d_int", (string) PERCORSO_BASE_DEFAULT, id);
   sprintf(pipe_esterna, "%s/%d_ext", (string) PERCORSO_BASE_DEFAULT, id);
 
@@ -41,11 +118,12 @@ int main( int argn, char** argv ){
 
 void ascolta_e_interpreta(){
 
+  // Leggo il messaggio.
   char messaggio_in[200];
   read_msg(pipe_interna, messaggio_in, 199);
-  //fgets(messaggio_in, 199, stdin);
   strtok(messaggio_in, "\n");
 
+  // Divido la stringa per gli spazi.
   coda_stringhe* separata = crea_coda_da_stringa(messaggio_in, " ");
 
   char comando[50];
@@ -53,136 +131,25 @@ void ascolta_e_interpreta(){
 
   if( strcmp( comando, GET_STATUS ) == 0 ){
 
-      char id_ric[50];
-      primo(separata, id_ric, TRUE);
-      int id_comp = atoi(id_ric);
-
-      if( id_comp == id || id_comp == ID_UNIVERSALE ){
-
-        char* response = (char*) malloc(sizeof(char) * 200 * lista_pipes -> n);
-        sprintf(response, "%s HUB id: %d\n" ,GET_STATUS_RESPONSE, id);
-        nodo_stringa* it = lista_pipes -> testa;
-        while( it != NULL ){
-
-          string pipe = it -> val;
-          char msg[200];
-          sprintf(msg, "%s %d", GET_STATUS, ID_UNIVERSALE );
-          if( send_msg(pipe, msg) == FALSE || read_msg(pipe, msg, 199) == FALSE  ){
-
-            nodo_stringa* tmp = it;
-            rimuovi_nodo(lista_pipes, it);
-            free(tmp);
-
-
-          } else {
-
-            printf("TRE\n");
-            strcat(response, msg+strlen(GET_STATUS_RESPONSE)+1);
-
-          }
-          it = it -> succ;
-
-
-        }
-        send_msg(pipe_interna, response);
-        free(response);
-
-    }
-
+    gestisci_STATUSGET(separata);
 
   } else if( strcmp(comando, UPDATE_LABEL) == 0 ){
 
-    char id_ric[50];
-    primo(separata, id_ric, TRUE);
-    int id_comp = atoi(id_ric);
-    if( id_comp == id || id_comp == ID_UNIVERSALE ){
-
-      char label[50];
-      primo(separata, label, TRUE);
-      char pos[50];
-      primo(separata, pos, TRUE);
-
-      char msg[200];
-      sprintf(msg, "%s %d %s %s", UPDATE_LABEL, ID_UNIVERSALE, label, pos);
-
-      nodo_stringa* it = lista_pipes -> testa;
-      while( it != NULL ){
-
-        string pipe = it -> val;
-        if( send_msg(pipe, msg) == FALSE ){
-
-          nodo_stringa* tmp = it;
-          rimuovi_nodo(lista_pipes, it);
-          it = it -> succ;
-          free(tmp);
-
-        }
-
-        it = it -> succ;
-
-      }
-
-    }
+    gestisci_LABELUP(separata);
 
   } else if( strcmp( comando, ID ) == 0 ) {
 
-    char id_ric[20];
-    primo(separata, id_ric, TRUE);
-    int id_comp = atoi(id_ric);
-    if( id_comp == id || id_comp == ID_UNIVERSALE )
-      send_msg(pipe_interna, "TRUE");
-    else
-      send_msg(pipe_interna, "FALSE");
+    gestisci_ID(separata);
 
   } else if( strcmp(comando, REMOVE) == 0 ){
 
-    char id_ric[20];
-    primo(separata, id_ric, TRUE);
-    int id_comp = atoi(id_ric);
-    if( id_comp == id || id_comp == ID_UNIVERSALE )
-      termina(0);
+    gestisci_REMOVE(separata);
 
   } else if( strcmp(comando, "SPAWN") == 0 ){
 
-    char id_ric[20];
-    primo(separata, id_ric, TRUE);
-    int id_comp = atoi(id_ric);
-    if( id_comp == id || id_comp == ID_UNIVERSALE ){
+    gestisci_SPAWN(separata);
 
-      char line[1024];
-      strcpy(line, "");
-      char type[100];
-      char tmp[100];
-
-      primo(separata, type, TRUE);
-      while( primo(separata, tmp, TRUE) ==  TRUE ){
-
-        strcat(line, tmp);
-        strcat(line, " ");
-
-      }
-      char path[200];
-      sprintf(path, "./%s.out", type );
-      pid_t pid = fork();
-
-      if( pid == 0 ){
-
-        execlp(path, path, line, NULL);
-
-      } else if( pid > 0 ) {
-
-        char pipe_figlio[100];
-        sprintf(pipe_figlio, "%s/%s", (string) PERCORSO_BASE_DEFAULT, strtok(line, " "));
-        append(lista_pipes, pipe_figlio);
-
-      }
-
-
-    }
-
-  }
-
-  else {
+  }  else {
 
     printf("Comando non supportato\n");
 
@@ -205,12 +172,16 @@ boolean calcola_registro_stringa( const registro* r, string output){
 
 void termina(int x){
 
+  // Uccido i miei figli
   kill(figli[0], SIGKILL);
   kill(figli[1], SIGKILL);
 
+  // Chiudo il file descriptor.
   close(file);
   char path[200];
   sprintf(path, "%s/%d", (string) PERCORSO_BASE_DEFAULT, id);
+
+  // Invio a tutti i miei figli il comando REMOVE
   nodo_stringa* it = lista_pipes -> testa;
   while( it != NULL ){
 
@@ -221,6 +192,8 @@ void termina(int x){
     it = it -> succ;
 
   }
+
+  // Distruggo tutte le pipe.
   unlink(path);
   unlink(pipe_esterna);
   unlink(pipe_interna);
@@ -233,6 +206,8 @@ void crea_processi_supporto(){
   pid_t pid = fork();
   if( pid == 0 ){
 
+    // Se sono il figlio sto perennemente in ascolto sulla pipe con l'umano
+    // e scrivo tutto su quella interna
     mkfifo(pipe_esterna, 0666);
     while(1){
 
@@ -248,6 +223,8 @@ void crea_processi_supporto(){
     pid = fork();
     if( pid == 0 ){
 
+      // Se sono il figlio leggo dalla pipe con il controllore e invio su quella
+      // interna, aspettando la risposta.
       while(1){
         crea_pipe(id, (string) PERCORSO_BASE_DEFAULT);
         char msg[200];
@@ -257,12 +234,11 @@ void crea_processi_supporto(){
         read_msg(pipe_interna, msg, 199);
         if( strcmp(msg, "DONE") != 0 )
           manda_messaggio(id, (string) PERCORSO_BASE_DEFAULT, msg);
-        else
-          printf("DONE\n");
       }
 
-    } else if( pid > 0 ) {
+    } else if( pid > 0 ) {
 
+      // Se sono il padre sto in ascolto sulla pipe interna e interpreto i messaggi.
       figli[1] = pid;
       signal(SIGINT, termina);
       mkfifo(pipe_interna, 0666);
@@ -273,5 +249,161 @@ void crea_processi_supporto(){
     }
 
   }
+
+}
+
+
+void gestisci_STATUSGET(coda_stringhe* separata){
+
+  char id_ric[50];
+  primo(separata, id_ric, TRUE);
+  int id_comp = atoi(id_ric);
+
+  if( id_comp == id || id_comp == ID_UNIVERSALE ){
+
+    // Creo il messaggio contenente la risposta.
+    char* response = (char*) malloc(sizeof(char) * 200 * lista_pipes -> n);
+    sprintf(response, "%s HUB id: %d\n" ,GET_STATUS_RESPONSE, id);
+
+    // Mando a tutti i miei figli che voglio lo stato.
+    nodo_stringa* it = lista_pipes -> testa;
+    while( it != NULL ){
+
+      string pipe = it -> val;
+      char msg[200];
+      sprintf(msg, "%s %d", GET_STATUS, ID_UNIVERSALE );
+      if( send_msg(pipe, msg) == FALSE || read_msg(pipe, msg, 199) == FALSE  ){
+
+        nodo_stringa* tmp = it;
+        rimuovi_nodo(lista_pipes, it);
+        free(tmp);
+
+
+      } else {
+
+        strcat(response, msg+strlen(GET_STATUS_RESPONSE)+1);
+
+      }
+      it = it -> succ;
+
+
+    }
+    // Rispondo sulla pipe_interna.
+    send_msg(pipe_interna, response);
+    free(response);
+
+} else
+  send_msg(pipe_interna, "DONE");
+
+}
+
+void gestisci_LABELUP(coda_stringhe* separata){
+
+  char id_ric[50];
+  primo(separata, id_ric, TRUE);
+  int id_comp = atoi(id_ric);
+  if( id_comp == id || id_comp == ID_UNIVERSALE ){
+
+    // Prendo il nome dell'interruttore e la nuova posizione.
+    char label[50];
+    primo(separata, label, TRUE);
+    char pos[50];
+    primo(separata, pos, TRUE);
+
+    // Invio il messaggio a tutti i miei figli.
+    char msg[200];
+    sprintf(msg, "%s %d %s %s", UPDATE_LABEL, ID_UNIVERSALE, label, pos);
+
+    nodo_stringa* it = lista_pipes -> testa;
+    while( it != NULL ){
+
+      string pipe = it -> val;
+      if( send_msg(pipe, msg) == FALSE ){
+
+        nodo_stringa* tmp = it;
+        rimuovi_nodo(lista_pipes, it);
+        it = it -> succ;
+        free(tmp);
+
+      }
+
+      it = it -> succ;
+
+    }
+
+  }
+  send_msg(pipe_interna, "DONE");
+
+}
+
+void gestisci_ID(coda_stringhe* separata){
+
+  // Recupero l'ID e rispondo se è il mio o no.
+  char id_ric[20];
+  primo(separata, id_ric, TRUE);
+  int id_comp = atoi(id_ric);
+  if( id_comp == id || id_comp == ID_UNIVERSALE )
+    send_msg(pipe_interna, "TRUE");
+  else
+    send_msg(pipe_interna, "FALSE");
+
+}
+
+void gestisci_REMOVE(coda_stringhe* separata){
+
+  // Recupero l'ID e in caso mi termino.
+  char id_ric[20];
+  primo(separata, id_ric, TRUE);
+  int id_comp = atoi(id_ric);
+  if( id_comp == id || id_comp == ID_UNIVERSALE )
+    termina(0);
+
+}
+
+void gestisci_SPAWN(coda_stringhe* separata){
+
+
+  char id_ric[20];
+  primo(separata, id_ric, TRUE);
+  int id_comp = atoi(id_ric);
+  if( id_comp == id || id_comp == ID_UNIVERSALE ){
+
+
+    char line[1024];
+    strcpy(line, "");
+    char type[100];
+    char tmp[100];
+
+    // Recupero il tipo di componente.
+    primo(separata, type, TRUE);
+    while( primo(separata, tmp, TRUE) ==  TRUE ){
+
+      // Creo il resto dei parametri da passare su linea di comando.
+      strcat(line, tmp);
+      strcat(line, " ");
+
+    }
+    char path[200];
+    sprintf(path, "./%s.out", type );
+    pid_t pid = fork();
+
+    if( pid == 0 ){
+
+      // Se sono il figlio cambio l'immagine.
+      execlp(path, path, line, NULL);
+
+    } else if( pid > 0 ) {
+
+      // Se sono il padre aggiungo alla mia lista di pipes la pipe del figlio appena creato.
+      char pipe_figlio[100];
+      sprintf(pipe_figlio, "%s/%s", (string) PERCORSO_BASE_DEFAULT, strtok(line, " "));
+      append(lista_pipes, pipe_figlio);
+
+    }
+
+
+  }
+
+  send_msg(pipe_interna, "DONE");
 
 }
